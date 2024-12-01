dd7ccd1c84a5c26a6e9ced0b468af9b427952e16
Acnologia7 <gamergamy666@gmail.com>
2024-12-01 16:16:39 +0100

    refactor: Changes structure of clustering module classes: 	- Implemented changes based of review comments for clustering module 	- Implemented abstract BaseCluster class for defining methods for subclasses and better docs organization 	- simlified docstrings

--- app/clustering.py
+++ app/clustering.py
@@ -1,15 +1,72 @@
+from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Literal, Optional, Union
 
 from numpy import ndarray
 from sklearn.cluster import DBSCAN, AgglomerativeClustering, KMeans
 
 
-class CKMeans:
+class BaseCluster(ABC):
     """
-    Utility class for KMeans clustering.
+    Abstract base class for clustering algorithms.
 
-    This class simplifies the process of using the KMeans algorithm
-    from scikit-learn, providing a wrapper around its implementation.
+    Subclasses must implement the `cluster` method to perform clustering.
+
+    Example:
+    ```python
+    class CustomCluster(BaseCluster):
+        def cluster(self, data: ndarray):
+            # Custom clustering logic here
+            pass
+    ```
+    """
+
+    @abstractmethod
+    def cluster(
+        self, data: ndarray, sample_weight: Optional[List[float]] = None
+    ) -> ndarray:
+        """
+        Perform clustering on the data and return cluster labels.
+
+        Args:
+            data (ndarray): Input data for clustering.
+            sample_weight (Optional[List[float]]): Weights for the samples.
+
+        Returns:
+            ndarray: Cluster labels for each data point.
+        """
+        pass
+
+
+class CKMeans(BaseCluster):
+    """
+    Utility class for KMeans clustering using scikit-learn.
+
+    Args:
+        n_clusters (int): The number of clusters to form.
+        init (str): Method for initialization ('k-means++' or 'random').
+        n_init (int or str): Number of time the k-means algorithm will be run.
+        max_iter (int): Maximum number of iterations for the algorithm.
+        tol (float): Tolerance to declare convergence.
+        verbose (int): Verbosity level.
+        random_state (Optional[int]): Random state for reproducibility.
+        copy_x (bool): Whether to copy data.
+        algorithm (str): Algorithm to use ('lloyd' or 'elkan').
+
+    Example:
+    ```python
+    kmeans = CKMeans(
+        n_clusters=3,
+        init='k-means++',
+        n_init=10,
+        max_iter=300,
+        tol=1e-4,
+        verbose=0,
+        random_state=42,
+        copy_x=True,
+        algorithm='lloyd'
+    )
+    labels = kmeans.cluster(data)
+    ```
     """
 
     def __init__(
@@ -24,45 +81,6 @@ class CKMeans:
         copy_x: bool,
         algorithm: Literal["lloyd", "elkan"],
     ) -> None:
-        """
-        Initialize the CKMeans class.
-
-        Args:
-            n_clusters (int): The number of clusters to form as well as
-            the number of centroids to generate.
-            init (Union[Literal["k-means++", "random"]]):
-            Method for initialization.
-                - "k-means++" ensures smarter centroid initialization.
-                - "random" selects initial centroids randomly.
-            n_init (Union[Literal["auto"], int]):
-            Number of times the algorithm
-            will be run with different centroid seeds.
-            max_iter (int):
-            Maximum number of iterations of the KMeans algorithm
-            for a single run.
-            tol (float): Relative tolerance to declare convergence.
-            verbose (int): Verbosity level for debugging. 0 = silent.
-            random_state (Optional[int]):
-            Seed for reproducibility. `None` for random.
-            copy_x (bool): Whether to make a copy of the input data.
-            algorithm (Literal["lloyd", "elkan"]):
-            KMeans algorithm variant to use.
-
-        Example usage:
-            ```python
-            from numpy import array
-            from your_module import CKMeans
-
-            data = array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-            kmeans = CKMeans(
-                n_clusters=2, init="k-means++", n_init="auto",
-                max_iter=300, tol=1e-4, verbose=0, random_state=42,
-                copy_x=True, algorithm="lloyd"
-            )
-            labels = kmeans.cluster(data)
-            print(labels)
-            ```
-        """
         self.model = KMeans(
             n_clusters=n_clusters,
             init=init,
@@ -78,33 +96,41 @@ class CKMeans:
     def cluster(
         self, data: ndarray, sample_weight: Optional[List[float]] = None
     ) -> ndarray:
-        """
-        Perform clustering on the input data and return the cluster labels.
-
-        Args:
-            data (Union[ndarray, list[list[float]]]):
-            Input data for clustering.
-            sample_weight (Optional[List[float]]):
-            Sample weights to influence clustering.
-
-        Returns:
-            ndarray: Cluster labels for each data point.
-
-        Example usage:
-            ```python
-            labels = kmeans.cluster(data)
-            print(labels)
-            ```
-        """
         return self.model.fit_predict(data, sample_weight=sample_weight)
 
 
-class CDBSCAN:
+class CDBSCAN(BaseCluster):
     """
-    Utility class for DBSCAN clustering.
+    Utility class for DBSCAN clustering using scikit-learn.
 
-    This class simplifies the process of using the DBSCAN algorithm
-    from scikit-learn, providing a wrapper around its implementation.
+    Args:
+        eps (float):
+        The maximum distance between two samples for
+        them to be considered as in the same neighborhood.
+        min_samples (int):
+        The number of samples in a neighborhood for
+        a point to be considered as a core point.
+        metric (str): The distance metric to use.
+        metric_params (Optional[Dict[str, Any]]):
+        Additional parameters for the metric.
+        algorithm (str):
+        Algorithm to compute nearest neighbors
+        ('auto', 'ball_tree', 'kd_tree', 'brute').
+        leaf_size (int): Leaf size for tree-based algorithms.
+        p (Optional[float]): Power parameter for Minkowski metric.
+        n_jobs (Optional[int]): The number of parallel jobs to run.
+
+    Example:
+    ```python
+    dbscan = CDBSCAN(
+        eps=0.5,
+        min_samples=5,
+        metric='euclidean',
+        algorithm='auto',
+        leaf_size=30
+    )
+    labels = dbscan.cluster(data)
+    ```
     """
 
     def __init__(
@@ -118,38 +144,6 @@ class CDBSCAN:
         p: Optional[float],
         n_jobs: Optional[int],
     ) -> None:
-        """
-        Initialize the CDBSCAN class.
-
-        Args:
-            eps (float): The maximum distance between two samples
-            for them to be considered as in the same neighborhood.
-            min_samples (int): The minimum number of samples in a neighborhood
-            for a point to be considered a core point.
-            metric (str): The metric to use for distance computation.
-            metric_params (Optional[Dict[str, Any]]):
-            Additional keyword arguments for the metric function.
-            algorithm (Literal["auto", "ball_tree", "kd_tree", "brute"]):
-            Algorithm used to compute nearest neighbors.
-            leaf_size (int): Leaf size for the tree-based algorithms.
-            p (Optional[float]): The power parameter for Minkowski metric.
-            n_jobs (Optional[int]): The number of parallel jobs.
-
-        Example usage:
-            ```python
-            from numpy import array
-            from your_module import CDBSCAN
-
-            data = array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-            dbscan = CDBSCAN(
-                eps=0.5, min_samples=2, metric="euclidean",
-                metric_params=None, algorithm="auto",
-                leaf_size=30, p=None, n_jobs=-1
-            )
-            labels = dbscan.cluster(data)
-            print(labels)
-            ```
-        """
         self.model = DBSCAN(
             eps=eps,
             min_samples=min_samples,
@@ -164,33 +158,41 @@ class CDBSCAN:
     def cluster(
         self, data: ndarray, sample_weight: Optional[List[float]] = None
     ) -> ndarray:
-        """
-        Perform clustering on the input data and return the cluster labels.
-
-        Args:
-            data (Union[ndarray, list[list[float]]]):
-            Input data for clustering.
-            sample_weight (Optional[List[float]]):
-            Sample weights to influence clustering.
-
-        Returns:
-            ndarray: Cluster labels for each data point.
-
-        Example usage:
-            ```python
-            labels = dbscan.cluster(data)
-            print(labels)
-            ```
-        """
         return self.model.fit_predict(data, sample_weight=sample_weight)
 
 
-class CAgglomerativeClustering:
+class CAgglomerativeClustering(BaseCluster):
     """
-    Utility class for Agglomerative Clustering.
+    Utility class for Agglomerative Clustering using scikit-learn.
 
-    This class simplifies the process of using the Agglomerative Clustering
-    algorithm from scikit-learn, providing a wrapper around its implementation.
+    Args:
+        n_clusters (int):
+        The number of clusters to find.
+        metric (str):
+        The metric used for distance computation.
+        memory (Optional[str]):
+        Directory to cache computed distances.
+        connectivity (Optional[List[Any]]):
+        Connectivity matrix for samples.
+        compute_full_tree (Union[Literal['auto'], bool]):
+        Whether to compute the full tree.
+        linkage (str):
+        The linkage criterion ('ward', 'complete', 'average', 'single').
+        distance_threshold (Optional[float]):
+        The linkage threshold.
+        compute_distances (bool):
+        Whether to compute distances between clusters.
+
+    Example:
+    ```python
+    agglo = CAgglomerativeClustering(
+        n_clusters=3,
+        metric='euclidean',
+        linkage='ward',
+        compute_distances=False
+    )
+    labels = agglo.cluster(data)
+    ```
     """
 
     def __init__(
@@ -204,41 +206,6 @@ class CAgglomerativeClustering:
         distance_threshold: Optional[float],
         compute_distances: bool,
     ) -> None:
-        """
-        Initialize the CAgglomerativeClustering class.
-
-        Args:
-            n_clusters (int): The number of clusters to find.
-            metric (str): The metric to use for computing distances.
-            memory (Optional[str]):
-            Cache to use for storing intermediate results.
-            connectivity (Optional[list[Any]]):
-            Connectivity matrix for samples.
-            compute_full_tree (Union[Literal["auto"], bool]):
-            Whether to compute the full tree.
-            linkage (Literal["ward", "complete", "average", "single"]):
-            Linkage criterion to use.
-            distance_threshold (Optional[float]):
-            Threshold for merging clusters.
-            compute_distances (bool):
-            Whether to compute distances between clusters.
-
-        Example usage:
-            ```python
-            from numpy import array
-            from your_module import CAgglomerativeClustering
-
-            data = array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-            agglomerative = CAgglomerativeClustering(
-                n_clusters=2, metric="euclidean", memory=None,
-                connectivity=None, compute_full_tree="auto",
-                linkage="ward", distance_threshold=None,
-                compute_distances=False
-            )
-            labels = agglomerative.cluster(data)
-            print(labels)
-            ```
-        """
         self.model = AgglomerativeClustering(
             n_clusters=n_clusters,
             metric=metric,
@@ -250,21 +217,12 @@ class CAgglomerativeClustering:
             compute_distances=compute_distances,
         )
 
-    def cluster(self, data: ndarray) -> ndarray:
-        """
-        Perform clustering on the input data and return the cluster labels.
+    def cluster(
+        self, data: ndarray, sample_weight: Optional[List[float]] = None
+    ) -> ndarray:
+        if sample_weight is not None:
+            raise NotImplementedError(
+                "CAgglomerativeClustering does not support `sample_weight`."
+            )
 
-        Args:
-            data (Union[ndarray, list[list[float]]]):
-            Input data for clustering.
-
-        Returns:
-            ndarray: Cluster labels for each data point.
-
-        Example usage:
-            ```python
-            labels = agglomerative.cluster(data)
-            print(labels)
-            ```
-        """
         return self.model.fit_predict(data)
